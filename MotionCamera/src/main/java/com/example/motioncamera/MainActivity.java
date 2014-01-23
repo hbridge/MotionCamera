a76c0b7b8e966d431265da8fce45f80fa135591a
Aseem Sood <asood123@yahoo.com>
2014-01-22 20:12:20 -0500

    Added Camera support. Takes a picture when you shake it.

--- MotionCamera/src/main/java/com/example/motioncamera/MainActivity.java
+++ MotionCamera/src/main/java/com/example/motioncamera/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.motioncamera;
 
+import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,18 +9,29 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.FloatMath;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
 
 public class MainActivity extends ActionBarActivity implements SensorEventListener {
 
     // Start with some variables
+    private static final String TAG = "MotionCamera";
     private SensorManager sensorMan;
     private Sensor accelerometer;
+    Camera camera;
+    Preview preview;
+    static boolean photoInProgress;
 
     private float[] mGravity;
     private float mAccel;
@@ -32,11 +44,11 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
 
 
         sensorMan = (SensorManager)getSystemService(SENSOR_SERVICE);
@@ -44,6 +56,10 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         mAccel = 0.00f;
         mAccelCurrent = SensorManager.GRAVITY_EARTH;
         mAccelLast = SensorManager.GRAVITY_EARTH;
+        photoInProgress = false;
+
+        preview = new Preview(this);
+        ((FrameLayout) findViewById(R.id.preview)).addView(preview);
 
     }
 
@@ -85,17 +101,59 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     }
 
 
+    Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            Log.d(TAG, "onShutter'd");
+
+        }
+    };
+
+    /** Handles data for raw picture */
+    Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "onPictureTaken - raw");
+            photoInProgress = false;
+            preview.camera.startPreview();
+        }
+    };
+
+    /** Handles data for jpeg picture */
+    Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            FileOutputStream outStream = null;
+            try {
+                outStream = new FileOutputStream(String.format(
+                        "/temp/%d.jpg", System.currentTimeMillis()));
+                outStream.write(data);
+                outStream.close();
+                Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.err.println("Error thrown");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                photoInProgress = false;
+                preview.camera.startPreview();
+            }
+            Log.d(TAG, "onPictureTaken - jpeg");
+
+        }
+    };
+
     @Override
     public void onResume() {
         super.onResume();
         sensorMan.registerListener(this, accelerometer,
                 SensorManager.SENSOR_DELAY_UI);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         sensorMan.unregisterListener(this);
+
     }
 
     @Override
@@ -115,6 +173,11 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
             if(mAccel > 3){
                 TextView textView = (TextView) findViewById(R.id.textField);
                 textView.setText(Float.toString(mAccel));
+                if (!photoInProgress) {
+                    photoInProgress = true;
+                    preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+                }
+
             }
         }
 
